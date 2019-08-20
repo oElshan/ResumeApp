f6946a021fba2f747acca3f52532b8935b388d29
oelshan <o.elshan@yandex.ru>
2019-08-21 00:00:13 +0900

    add profile html

---
+++ src/main/java/isha/resume/controllers/SerchConroller.java
@@ -0,0 +1,27 @@
+package isha.resume.controllers;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/profile")
+public class SerchConroller extends HttpServlet {
+    private final static Logger LOGGER = Logger.getLogger(SerchConroller.class);
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/JSP/profile.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
